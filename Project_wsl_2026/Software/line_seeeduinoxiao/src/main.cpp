9587c8c300541c7cd984c2bcb83df20be763e2fc
miso.soup_q <kamirobot1031@gmail.com>
2025-08-05 02:05:50 +0900

    tired

--- Project_wsl_2026/Software/line_seeeduinoxiao/src/main.cpp
+++ Project_wsl_2026/Software/line_seeeduinoxiao/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
+#include "multiplexer.hpp"
 
-// put function declarations here:
-int myFunction(int, int);
+Multiplexer line_mux; // 定義
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  Serial.begin(9600);
+
+  line_mux.set_pin(1, 2, 3, 4, 5, -1); // ピンを指定
+  line_mux.init(10);                   // 時間を指定
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  for (int i = 0; i < 16; i++)
+  {
+    Serial.print("index");
+    Serial.print(i);
+    Serial.print(" : ");
+    Serial.print(line_mux.read(i)); // 読み取る
+    Serial.print(" , ");
+  }
+  Serial.println(); // 改行
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(10); // 10ms待機
 }
