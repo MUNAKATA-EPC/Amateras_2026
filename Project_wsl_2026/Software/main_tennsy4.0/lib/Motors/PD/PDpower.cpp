1bfbb7b392a2caa04166ebfe4222da9fdd3bcdab
miso.soup_q <kamirobot1031@gmail.com>
2025-08-09 23:03:26 +0900

    pd

--- Project_wsl_2026/Software/main_tennsy4.0/lib/Motors/PD/PDpower.cpp
+++ Project_wsl_2026/Software/main_tennsy4.0/lib/Motors/PD/PDpower.cpp
@@ -5,10 +5,14 @@
 #define GYRO_P_GAIN 0.6
 #define GYRO_D_GAIN 0.02
 
+#define GYRO_TARGET_P_GAIN 0.6
+#define GYRO_TARGET_D_GAIN 0.02
+
 #define GOAL_P_GAIN 0.35
 #define GOAL_D_GAIN 0.02
 
-int pd_power;            // PD制御の出力格納用
+int pd_power; // PD制御の出力格納用
+
 double p_power, d_power; // P・Dそれぞれの出力格納
 
 int now_gyro_value = 0;    // 今の角度格納用(-180~180)
@@ -21,8 +25,14 @@ bool gyro_first_call = true; // 関数が最初に呼び出されたかを読む
 void PD_use_gyro()
 {
     now_gyro_value = (get_BNO055_deg() < 180) ? get_BNO055_deg() : get_BNO055_deg() - 360; // BNO055の角度を-180~180にして保存
-    gap_of_gyro_value = now_gyro_value - old_gyro_value;                                   // 差を計算
-    old_gyro_value = now_gyro_value;                                                       // BNO055の昔の角度を-180~180にして保存
+
+    gap_of_gyro_value = now_gyro_value - old_gyro_value; // 差を計算
+    if (gap_of_gyro_value > 180)
+        gap_of_gyro_value -= 360; // 補正
+    if (gap_of_gyro_value < -180)
+        gap_of_gyro_value += 360; // 補正
+
+    old_gyro_value = now_gyro_value; // BNO055の昔の角度を-180~180にして保存
 
     /*P制御について*/
     p_power = now_gyro_value * GYRO_P_GAIN; // 角度のずれ様によって比例制御
@@ -56,44 +66,59 @@ void PD_use_gyro()
     pd_power = constrain(pd_power, -100, 100); // 一応-100~100に収める
 }
 
+double set_target_p_power, set_target_d_power; // P・Dそれぞれの出力格納
+
+int now_set_target_gyro_value = 0;    // 今の角度格納用(-180~180)
+int old_set_target_gyro_value = 0;    // 昔の角度格納用(-180~180)
+int gap_of_set_target_gyro_value = 0; // 今と昔の差格納用(-180~180)
+
+Timer set_target_gyro_timer;            // ジャイロ用のタイマーを定義
+bool set_target_gyro_first_call = true; // 関数が最初に呼び出されたかを読む
+
 void PD_use_gyro_set_target_ver(int target_deg)
 {
-    target_deg = (target_deg + 360) % 360;                                                                               // 一応0~360度にする
-    int BNO055_deg_with_target_deg = (get_BNO055_deg() - target_deg + 360) % 360;                                        // target_degからどれだけ離れているか計算
-    now_gyro_value = (BNO055_deg_with_target_deg < 180) ? BNO055_deg_with_target_deg : BNO055_deg_with_target_deg - 360; // BNO055の角度を-180~180にして保存
-    gap_of_gyro_value = now_gyro_value - old_gyro_value;                                                                 // 差を計算
-    old_gyro_value = now_gyro_value;                                                                                     // BNO055の昔の角度を-180~180にして保存
+    target_deg = (target_deg + 360) % 360;                                                                                          // 一応0~360度にする
+    int BNO055_deg_with_target_deg = (get_BNO055_deg() - target_deg + 360) % 360;                                                   // target_degからどれだけ離れているか計算
+    now_set_target_gyro_value = (BNO055_deg_with_target_deg < 180) ? BNO055_deg_with_target_deg : BNO055_deg_with_target_deg - 360; // BNO055の角度を-180~180にして保存
+
+    gap_of_set_target_gyro_value = now_set_target_gyro_value - old_set_target_gyro_value; // 差を計算
+    if (gap_of_set_target_gyro_value > 180)
+        gap_of_set_target_gyro_value -= 360; // 補正
+    if (gap_of_set_target_gyro_value < -180)
+        gap_of_set_target_gyro_value += 360; // 補正
+
+    old_set_target_gyro_value = now_set_target_gyro_value; // BNO055の昔の角度を-180~180にして保存
 
     /*P制御について*/
-    p_power = now_gyro_value * GYRO_P_GAIN; // 角度のずれ様によって比例制御
+    set_target_p_power = now_set_target_gyro_value * GYRO_TARGET_P_GAIN; // 角度のずれ様によって比例制御
 
     /*D制御について*/
-    if (gyro_first_call)
+    if (set_target_gyro_first_call)
     {
-        gyro_timer.reset();      // タイマーをリセット
-        gyro_first_call = false; // 次からは初回ではない
+        set_target_gyro_timer.reset();      // タイマーをリセット
+        set_target_gyro_first_call = false; // 次からは初回ではない
 
-        d_power = 0.0; // 最初の呼び出しなので値は0
+        set_target_d_power = 0.0; // 最初の呼び出しなので値は0
     }
     else
     {
-        double gyro_timer_sec = gyro_timer.get_time() / 1000.0; // msからsに変える
+        double set_target_gyro_timer_sec = set_target_gyro_timer.get_time() / 1000.0; // msからsに変える
 
-        if (gyro_timer_sec > 1e-6)
+        if (set_target_gyro_timer_sec > 1e-6)
         {
-            d_power = GYRO_D_GAIN * gap_of_gyro_value / gyro_timer_sec; // deg/sec * GYRO_D_GAINでD制御の出力を求める
+            set_target_d_power = GYRO_TARGET_D_GAIN * gap_of_set_target_gyro_value / set_target_gyro_timer_sec; // deg/sec * GYRO_D_GAINでD制御の出力を求める
         }
         else
         {
-            d_power = 0.0; // 除算はだめなので0にする
+            set_target_d_power = 0.0; // 除算はだめなので0にする
         }
 
-        gyro_timer.reset(); // タイマーをリセット
+        set_target_gyro_timer.reset(); // タイマーをリセット
     }
 
     /*合計*/
-    pd_power = (int)(p_power + d_power);       // それぞれの出力を足し合わせる
-    pd_power = constrain(pd_power, -100, 100); // 一応-100~100に収める
+    pd_power = (int)(set_target_p_power + set_target_d_power); // それぞれの出力を足し合わせる
+    pd_power = constrain(pd_power, -100, 100);                 // 一応-100~100に収める
 }
 
 double yellow_goal_p_power, yellow_goal_d_power; // P成分・D成分
@@ -110,7 +135,13 @@ void PD_use_yellow_goal()
     if (is_yellow_goal_exist())
     {
         now_yellow_goal_value = (get_yellow_goal_deg() < 180) ? get_yellow_goal_deg() : get_yellow_goal_deg() - 360; // 現在の角度
-        gap_of_yellow_goal_value = now_yellow_goal_value - old_yellow_goal_value;                                    // 差を計算
+
+        gap_of_yellow_goal_value = now_yellow_goal_value - old_yellow_goal_value; // 差を計算
+        if (gap_of_yellow_goal_value > 180)
+            gap_of_yellow_goal_value -= 360; // 補正
+        if (gap_of_yellow_goal_value < -180)
+            gap_of_yellow_goal_value += 360; // 補正
+
         old_yellow_goal_value = now_yellow_goal_value;
 
         /*P制御*/
@@ -166,7 +197,13 @@ void PD_use_blue_goal()
     if (is_blue_goal_exist())
     {
         now_blue_goal_value = (get_blue_goal_deg() < 180) ? get_blue_goal_deg() : get_blue_goal_deg() - 360; // 現在の角度
-        gap_of_blue_goal_value = now_blue_goal_value - old_blue_goal_value;                                  // 差を計算
+
+        gap_of_blue_goal_value = now_blue_goal_value - old_blue_goal_value; // 差を計算
+        if (gap_of_blue_goal_value > 180)
+            gap_of_blue_goal_value -= 360; // 補正
+        if (gap_of_blue_goal_value < -180)
+            gap_of_blue_goal_value += 360; // 補正
+
         old_blue_goal_value = now_blue_goal_value;
 
         /*P制御*/
