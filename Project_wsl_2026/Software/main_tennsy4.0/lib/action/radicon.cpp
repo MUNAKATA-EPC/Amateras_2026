8dcb31788bad80ae37280a2767e9a69c393e4e2e
miso.soup_q <kamirobot1031@gmail.com>
2025-08-18 17:43:00 +0900

    radicon

--- Project_wsl_2026/Software/main_tennsy4.0/lib/action/radicon.cpp
+++ Project_wsl_2026/Software/main_tennsy4.0/lib/action/radicon.cpp
@@ -17,16 +17,17 @@ void play_radicon(int motor_power)
 
     /*ロボット制御*/
     int motor_power_variable = map(get_Ps3_stick_left_distance(), 0, 128, 0, motor_power); // 左ステックの倒されようによってスピードを変える
-    if (!get_Ps3_button_circle())
+    /*if (!get_Ps3_button_circle())
         motor_power_variable *= 0.75; // ◎ボタンが押されていないなら減速する
+    */
 
     if (is_LINE_exist()) // ラインがあるならば
     {
-        motors_move(get_LINE_memory_deg() + 180, motor_power_variable); // ラインから逃れる動きをする
+        motors_move(get_LINE_memory_deg() + 180, motor_power); // ラインから逃れる動きをする
     }
     else if (is_IR_exist() && get_Ps3_button_l3()) // IRボールがあってl3ボタンが押されたら
     {
-        play_attacker(false, false, motor_power_variable); // 攻撃モードに切り替える
+        play_attacker(false, false, motor_power); // 攻撃モードに切り替える
     }
     else if (is_Ps3_stick_left_move()) // 左スティックが倒されたならば
     {
@@ -34,6 +35,15 @@ void play_radicon(int motor_power)
     }
     else
     {
-        motors_only_PD(motor_power_variable); // PD制御のみで動く
+        motors_only_PD(motor_power); // PD制御のみで動く
     }
+
+    /*シリアルプリント*/
+    Serial.print(get_Ps3_stick_lx());
+    Serial.print("  ");
+    Serial.print(get_Ps3_stick_ly());
+    Serial.print("  ");
+    Serial.print(get_Ps3_stick_left_deg());
+    Serial.print("  ");
+    Serial.println(get_Ps3_stick_left_distance());
 }
--- Project_wsl_2026/Software/main_tennsy4.0/src/main.cpp
+++ Project_wsl_2026/Software/main_tennsy4.0/src/main.cpp
@@ -21,7 +21,7 @@ void setup()
   LINE_init(&Serial5, 115200); // シリアル5を使いボートレート115200にする
 
   DSR1202_set_motormove_togglepin(5); // モータを動かすトグルスイッチのピン番号を設定
-  DSR1202_init(&Serial1, 115200);     // シリアル2を使いボートレート115200にする
+  DSR1202_init(&Serial1, 115200);     // シリアル1を使いボートレート115200にする
   motors_init(315, 45, 225, 135);     // モーターの設置角度を定義
 
   Camera_init(&Serial3, 115200); // シリアル3を使いボートレート115200にする
@@ -33,8 +33,8 @@ void setup()
   kicker_set_fetpin(2, 3); // キッカーのFETピンを設定
   kicker_init(700);        // クールダウン時間の定義
 
-  // Ps3_set_stick_adjust(5, 5, 5, 5);
-  // Ps3_init(&Serial2, 115200); // シリアル4を使いボートレート115200にする
+  Ps3_set_stick_adjust(5, 5, 5, 5);
+  Ps3_init(&Serial2, 115200); // シリアル2を使いボートレート115200にする
 
   SSD1306_init(&Wire1, 0x3C, 128, 64);
   SSD1306_clear();
@@ -51,8 +51,8 @@ void loop()
   LINE_compute_update(); // 更新2
   Camera_update();       // 更新
   BNO055_update();       // 更新
-  // Ps3_serial_update();   // 更新1
-  // Ps3_compute_update();  // 更新2
+  Ps3_serial_update();   // 更新1
+  Ps3_compute_update();  // 更新2
 
   ui_process(); // uiを実行
 
@@ -64,7 +64,7 @@ void loop()
   }
   else // 今選んだ
   {
-    play_pc_print(ALL_CHECK_WITH_PC); // 全て確認する
+    // play_pc_print(ALL_CHECK_WITH_PC); // 全て確認する
 
     switch (get_selected_ui_action())
     {
