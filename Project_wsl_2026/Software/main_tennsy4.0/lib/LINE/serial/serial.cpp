bbb0b068a50d8377bfd6c6f9d664ef60efb55eff
miso.soup_q <kamirobot1031@gmail.com>
2025-08-22 01:17:19 +0900

    line revolution

--- Project_wsl_2026/Software/main_tennsy4.0/lib/LINE/serial/serial.cpp
+++ Project_wsl_2026/Software/main_tennsy4.0/lib/LINE/serial/serial.cpp
@@ -2,12 +2,14 @@
 
 /*LINEセンサーの状況をシリアル通信で受信する*/
 
+const uint8_t head_byte = 0xAA; // 同期ヘッダー格納用
+
 HardwareSerial *line_serial; // とりあえず定義
 uint32_t line_baudrate;      // ボートレート格納用
 
 bool line_exist = false; // ラインがあるかどうか
 
-uint16_t lines_data_bit_mask = 0; // 16個のラインセンサーの状況格納用
+uint32_t lines_data_bit_mask = 0; // 16+3個のラインセンサーの状況格納用
 
 void LINE_init(HardwareSerial *serial, uint32_t baudrate)
 {
@@ -18,19 +20,30 @@ void LINE_init(HardwareSerial *serial, uint32_t baudrate)
 
 void LINE_serial_update()
 {
-    if ((*line_serial).available() > 0)
+    if ((*line_serial).available()) // 受信バッファが溜まっているなら
     {
-        uint8_t low = (*line_serial).read();     // ボタンの下位バイトを読み取る
-        uint8_t high = (*line_serial).read();    // ボタンの上位バイトを読み取る
-        lines_data_bit_mask = (high << 8) | low; // 上位バイトと下位バイトをつなげる
+        if ((*line_serial).peek() == head_byte) // 最初のブッファが同期ヘッダーなら
+        {
+            if ((*line_serial).available() >= 3 + 1) // seeeduinoxiaoからの3つのデータと1つの同期ヘッダーが溜まっているなら
+            {
+                (*line_serial).read(); // 同期ヘッダーを捨てる
 
-        if (lines_data_bit_mask > 0) // 一個でも1があったら0より大きくなる
-            line_exist = true;
-        else
-            line_exist = false;
+                uint8_t low = (*line_serial).read();                      // ボタンの下位バイトを読み取る
+                uint8_t middle = (*line_serial).read();                   // ボタンの中位バイトを読み取る
+                uint8_t high = (*line_serial).read();                     // ボタンの上位バイトを読み取る
+                lines_data_bit_mask = (high << 16) | (middle << 8) | low; // 上位バイトと中位バイトと下位バイトをつなげる
 
-        while ((*line_serial).available())
-            (*line_serial).read(); // 残りのブッファを捨てる
+                if (lines_data_bit_mask > 0) // 一個でも1があったら0より大きくなる
+                    line_exist = true;
+                else
+                    line_exist = false;
+            }
+        }
+        else // そうでないならゴミのバッファ
+        {
+            while ((*line_serial).available())
+                (*line_serial).read(); // ブッファを捨てる
+        }
     }
 }
 
