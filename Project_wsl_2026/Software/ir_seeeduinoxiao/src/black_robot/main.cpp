0825760d4bb47bb8aed1ca69e14c823f8541ef80
miso.soup_q <kamirobot1031@gmail.com>
2025-08-22 16:52:31 +0900

    ir revolution

--- Project_wsl_2026/Software/ir_seeeduinoxiao/src/black_robot/main.cpp
+++ Project_wsl_2026/Software/ir_seeeduinoxiao/src/black_robot/main.cpp
@@ -3,16 +3,18 @@
 
 /*黒ロボット用*/
 
-Multiplexer ir_mux;
+const uint8_t head_byte = 0xAA; // 同期ヘッダー格納用
 
-const int IRsensor_pin[16] = {0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}; // 前から反時計回りに指定
+const uint8_t IRsensor_pin[16] = {0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}; // 前から反時計回りに指定
 
-int IRsensor_value[16]; // IRセンサーの値を格納
-int IRsensor_min_index; // IRセンサーで最も反応の小さいセンサーの配列番号
+int16_t IRsensor_value[16]; // IRセンサーの値を格納
+int16_t IRsensor_min_index; // IRセンサーで最も反応の小さいセンサーの配列番号
 
 double IRball_of_x, IRball_of_y; // IRセンサーのxy方向の重み
-int IRball_deg;                  // 角度
-int IRball_distance;             // 距離
+int16_t IRball_deg;              // 角度
+int16_t IRball_distance;         // 距離
+
+Multiplexer ir_mux;
 
 void setup()
 {
@@ -26,14 +28,12 @@ void setup()
 void loop()
 {
   IRsensor_min_index = 0; // 初期化
-  for (int i = 0; i < 16; i++)
+  for (uint8_t i = 0; i < 16; i++)
   {
     IRsensor_value[i] = ir_mux.read(IRsensor_pin[i]);
 
     if (IRsensor_value[i] < IRsensor_value[IRsensor_min_index])
-    {
       IRsensor_min_index = i;
-    }
   }
 
   int IRsensor_min_around_index[7] = {
@@ -55,7 +55,7 @@ void loop()
   {
     IRball_of_x = 0; // 初期化
     IRball_of_y = 0; // 初期化
-    for (int i = 0; i < 7; i++)
+    for (uint8_t i = 0; i < 7; i++)
     {
       IRball_of_x += cos(radians(IRsensor_min_around_index[i] * 22.5)) * (1023.0 - IRsensor_value[IRsensor_min_around_index[i]]); // 重みを加算
       IRball_of_y += sin(radians(IRsensor_min_around_index[i] * 22.5)) * (1023.0 - IRsensor_value[IRsensor_min_around_index[i]]); // 重みを加算
@@ -63,24 +63,27 @@ void loop()
 
     double IRball_deg_sub = degrees(atan2(IRball_of_y, IRball_of_x)); // 角度を算出
     IRball_deg_sub = (IRball_deg_sub < 0) ? IRball_deg_sub + 360 : IRball_deg_sub;
-    IRball_deg = (int)IRball_deg_sub;
+    IRball_deg = (int16_t)IRball_deg_sub;
     // IRball_distance = (int)(map(IRsensor_value[IRsensor_min_index], 0, 1023, 0, 200));
     double IRball_of_x_for_distance = IRball_of_x / 10.0;
     double IRball_of_y_for_distance = IRball_of_y / 10.0;
     double IRball_distance_sub = sqrt(IRball_of_x_for_distance * IRball_of_x_for_distance + IRball_of_y_for_distance * IRball_of_y_for_distance); // 距離を算出
-    IRball_distance = (int)map(IRball_distance_sub, 0, 500, 500, 0);
+    IRball_distance = (int16_t)map(IRball_distance_sub, 0, 500, 500, 0) - 20;
   }
+
+  /*送信*/
+  Serial1.write(head_byte);            // teensyとの通信開始
+  Serial1.write(IRball_deg);           // 2byteのデータなので下位の1byteのみ送信
+  Serial1.write(IRball_deg >> 8);      // 2byteのデータなので上位の1byteのみ送信
+  Serial1.write(IRball_distance);      // 2byteのデータなので下位の1byteのみ送信
+  Serial1.write(IRball_distance >> 8); // 2byteのデータなので上位の1byteのみ送信
+
+  Serial1.flush(); // 送信バッファがなくなるまで、つまり全て送信するまで待つ
 
   Serial.print(IRball_deg);
   Serial.print("a");
   Serial.print(IRball_distance);
   Serial.println("b");
-
-  Serial1.print(IRball_deg);
-  Serial1.print("a");
-  Serial1.print(IRball_distance);
-  Serial1.print("b");
-  Serial1.flush(); // シリアル通信のバッファをフラッシュ
 
   delay(10);
 }
