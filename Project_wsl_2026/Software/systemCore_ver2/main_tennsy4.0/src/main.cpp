24ca1f7453681b87b3f3d806597d1b78fc8e3f9c
miso.soup_q <kamirobot1031@gmail.com>
2025-09-02 18:45:36 +0900

    sensor

--- Project_wsl_2026/Software/systemCore_ver2/main_tennsy4.0/src/main.cpp
+++ Project_wsl_2026/Software/systemCore_ver2/main_tennsy4.0/src/main.cpp
@@ -4,38 +4,37 @@
 #include "Sensors.hpp"
 
 // 使用するセンサー類
-BallSensor ir;   // IRセンサー
-LineSensor line; // ラインセンサー
-Openmv cam;      // カメラ
-
-Button enterButton; // Uiの決定ボタン
-Button rightButton; // Uiの右ボタン
-Button leftButton;  // Uiの左ボタン
-
-Button resetButton; // ジャイロセンサーのリセットボタン
-BNO055 bno;         // ジャイロセンサー
-
+BallSensor ir;             // IRセンサー
+LineSensor line;           // ラインセンサー
+Openmv cam;                // カメラ
+Button enterButton;        // ディスプレイの決定ボタン
+Button rightButton;        // ディスプレイの右ボタン
+Button leftButton;         // ディスプレイの左ボタン
+Button resetButton;        // ジャイロセンサーのリセットボタン
+BNO055 bno;                // ジャイロセンサー
 DigitalSensor catchSensor; // キッカーのキャッチセンサー
 DigitalSensor motorToggle; // モータのトグルスイッチ
 
 void setup()
 {
+    // PCとのシリアル通信の定義
     Serial.begin(9600);
-
+    // IRセンサーの定義
     ir.init(&Serial1, 115200, 0xAA);
+    // ラインセンサーの定義
     line.init(&Serial2, 115200, 0xAA);
-
-    catchSensor.init(6);
-    motorToggle.init(5);
-
+    // カメラの定義
+    cam.init(&Serial3, 115200, 0xAA);
+    // ディスプレイの定義
     enterButton.init(11, INPUT_PULLDOWN);
     rightButton.init(12, INPUT_PULLDOWN);
     leftButton.init(10, INPUT_PULLDOWN);
-
+    // ジャイロの定義
     resetButton.init(9, INPUT_PULLDOWN);
     bno.init(&Wire1, 0x28, &resetButton);
-
-    cam.init(&Serial3, 115200, 0xAA);
+    // その他のセンサー
+    catchSensor.init(6);
+    motorToggle.init(5);
 }
 
 void loop()
@@ -46,7 +45,7 @@ void loop()
     line.update();
     // カメラの更新
     cam.update();
-    //  Uiの更新
+    //  ディスプレイの更新
     enterButton.update();
     rightButton.update();
     leftButton.update();
