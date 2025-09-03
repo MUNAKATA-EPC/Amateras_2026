195748586865b834e25e812c9cf93ee15a65db08
miso.soup_q <kamirobot1031@gmail.com>
2025-09-03 18:51:03 +0900

    kick

---
+++ Project_wsl_2026/Software/systemCore_ver2/main_tennsy4.0/lib/Drivers/Kicker/Kicker.cpp
@@ -0,0 +1,33 @@
+#include "Kicker.hpp"
+
+Kicker::Kicker(uint8_t kickpin, uint8_t chargepin)
+{
+    _kickpin = kickpin;
+    _chargepin = chargepin;
+}
+
+void Kicker::begin(unsigned long cooldownTime)
+{
+    _cooldownTime = cooldownTime;
+    pinMode(_kickpin, OUTPUT);
+}
+
+void Kicker::kick(bool signal)
+{
+    if (signal && (_kickTimer.msTime() > _cooldownTime || !_kickTimer.everCalled()))
+    {
+        digitalWrite(_kickpin, HIGH);
+        digitalWrite(_chargepin, LOW);
+        _kickTimer.reset();
+    }
+    else if (_kickTimer.msTime() <= 50)
+    {
+        digitalWrite(_kickpin, HIGH);
+        digitalWrite(_chargepin, LOW);
+    }
+    else
+    {
+        digitalWrite(_kickpin, LOW);
+        digitalWrite(_chargepin, HIGH);
+    }
+}
--- Project_wsl_2026/Software/systemCore_ver2/main_tennsy4.0/lib/Drivers/Kicker/Kicker.hpp
+++ Project_wsl_2026/Software/systemCore_ver2/main_tennsy4.0/lib/Drivers/Kicker/Kicker.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
 #include <Arduino.h>
+#include "Functions.hpp"
 
 class Kicker
 {
 private:
     uint8_t _kickpin;
     uint8_t _chargepin;
+    unsigned long _cooldownTime;
+    Timer _kickTimer;
 
 public:
     // 関数・コントラクタ
-    Kicker(uint8_t kickpin, uint8_t chergepin); // 定義
+    Kicker(uint8_t kickpin, uint8_t chargepin); // 定義
     void begin(unsigned long cooldownTime);     // 開始
     void kick(bool signal);                     // キックする
 };
