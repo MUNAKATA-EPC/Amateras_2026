ac89e9a0fe30390c860e0135c2869f2367640818
miso.soup_q <kamirobot1031@gmail.com>
2025-12-07 15:42:44 +0900

    hello

--- Project_wsl_2026/Software/system_core_ver1/line_seeeduinoxiao/src/auto_adjust_ver2/main.cpp
+++ Project_wsl_2026/Software/system_core_ver1/line_seeeduinoxiao/src/auto_adjust_ver2/main.cpp
@@ -11,14 +11,14 @@ const int end_header = 0xAA;   // 同期ヘッダー格納用
 Multiplexer line_mux;
 
 const int LINEsensor_pin[LINE_SENSOR_COUNT] = {0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1}; // 前から左回りにピン番号を指定
+float LINEsensor_x[LINE_SENSOR_COUNT];                                                                // 前をx正方向
+float LINEsensor_y[LINE_SENSOR_COUNT];                                                                // 前をx正方向
 
 const int button_pin = 0; // ボタンのピン番号
 const int led_pin = 7;    // LEDのピン番号
 
-float LINEsensor_x[LINE_SENSOR_COUNT]; // 前をx正方向
-float LINEsensor_y[LINE_SENSOR_COUNT]; // 前をx正方向
-
 Button button;
+Timer led_timer;
 
 void setup()
 {
@@ -38,22 +38,26 @@ void setup()
 
   button.init(button_pin, INPUT_PULLUP); // ボタン初期化
   pinMode(led_pin, OUTPUT);              // LEDピン初期化
-}
 
-// ボタンのコンディション格納用
-int button_state = 0;
-enum STATE
-{
-  COURT_ADJUST,
-  LINE_ADJUST,
-  COUNT
-};
+  // 一定時間以内にボタンを押すとセンサーの状況を送信するようになる
+  const float led_bright_hz = 2.0f; // 1s間に二周する
+  bool adjust_flag = false;
+
+  led_timer.reset();
+  while (led_timer.msTime() < 3000UL) // 3秒待機
+  {
+    button.update();
+
+    if (button.isReleased()) // ボタンが押されたら
+    {
+      adjust_flag = true;
+      break;
+    }
+
+    digitalWrite(led_pin, HIGH); // LED ON
+  }
+}
 
 void loop()
 {
-  button.update();
-
-  if (button.isReleased())
-    button_state += 1;
-  button_state %= STATE::COUNT;
 }
--- Project_wsl_2026/Software/system_core_ver1/main_tennsy4.0/mine/lib/action/attacker.cpp
+++ Project_wsl_2026/Software/system_core_ver1/main_tennsy4.0/mine/lib/action/attacker.cpp
@@ -5,5 +5,4 @@ static PD pdGyro(0.55, 1.6); // ジャイロ用のPD調節値
 void playAttacker(Attacker::Mode mode)
 {
     motorsPdProcess(&pdGyro, bnoDeg(), 0); // ジャイロで姿勢制御
-    motorsPdMove();
 }
